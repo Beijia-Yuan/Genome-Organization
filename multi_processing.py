15140238acd660158aaf06395ba6ca34f6ae58a6
Beijia-Yuan <74159056+Beijia-Yuan@users.noreply.github.com>
2023-04-04 20:05:45 +0200

    Update multi_processing.py

--- multi_processing.py
+++ multi_processing.py
@@ -8,8 +8,9 @@ import numpy as np
 import random
 from scipy.integrate import quad
 import matplotlib.pylab as plt
-from tqdm import tqdm
-from multiprocessing import Process
+from tqdm import tqdm 
+import multiprocess as mp
+from itertools import repeat
 
 # In[250]:
 
@@ -71,92 +72,97 @@ def pair_correlation(m,l,n,x,y):
 # In[227]:
 
 
-def generate_step(steps):
-    nxt = np.random.randint(6)
-    if nxt == 0:
-        steps.append(steps[-1]+np.array((1,0,0)))
-    elif nxt == 1:
-        steps.append(steps[-1]+np.array((-1,0,0)))
-    elif nxt == 2:
-        steps.append(steps[-1]+np.array((0,1,0)))
-    elif nxt == 3:
-        steps.append(steps[-1]+np.array((0,-1,0)))
-    elif nxt == 4:
-        steps.append(steps[-1]+np.array((0,0,1)))
-    elif nxt == 5:
-        steps.append(steps[-1]+np.array((0,0,-1)))
-
-
 def generateData(distribution):
-    for i in range(3_000_000):
-        steps = [np.array((0,0,0))]
-        for j in range(1000):
-            generate_step(steps)
-        a = steps[300]
-        b = steps[500]-steps[300]
-        c = steps[1000]-steps[500]
-        Imme = []
-        for j in range(300):
-            sj = steps[j+1]-steps[j]
-            for k in range(j+1,300):
-                sk = steps[k+1]-steps[k]
-                Imme.append(dot_prod(sj,sk))
-        x = tuple(a+b)
-        y = tuple(b+c)
-        if (x,y) in distribution:
-            distribution[(x,y)].append(np.mean(Imme))
-        else:
-            distribution[(x,y)] = [np.mean(Imme)]
+    import numpy as np
+    def dot_prod(x,y):
+        try:
+            iter(x)
+            if len(x) != len(y):
+                print("error_dot_product")
+                return
+            return (sum(x[i]*y[i] for i in range(len(x))))
+        except TypeError:
+            return x*y
+    def generate_step(steps):
+        nxt = np.random.randint(6)
+        if nxt == 0:
+            steps.append(steps[-1]+np.array((1,0,0)))
+        elif nxt == 1:
+            steps.append(steps[-1]+np.array((-1,0,0)))
+        elif nxt == 2:
+            steps.append(steps[-1]+np.array((0,1,0)))
+        elif nxt == 3:
+            steps.append(steps[-1]+np.array((0,-1,0)))
+        elif nxt == 4:
+            steps.append(steps[-1]+np.array((0,0,1)))
+        elif nxt == 5:
+            steps.append(steps[-1]+np.array((0,0,-1)))
+    steps = [np.array((0,0,0))]
+    for j in range(1000):
+        generate_step(steps)
+    a = steps[300]
+    b = steps[500]-steps[300]
+    c = steps[1000]-steps[500]
+    Imme = []
+    for j in range(300):
+        sj = steps[j+1]-steps[j]
+        for k in range(j+1,300):
+            sk = steps[k+1]-steps[k]
+            Imme.append(dot_prod(sj,sk))
+    x = tuple(a+b)
+    y = tuple(b+c)
+    if (x,y) in distribution:
+        distribution[(x,y)].append(np.mean(Imme))
+    else:
+        distribution[(x,y)] = [np.mean(Imme)]
 
 
 # In[231]:
 
 
-distribution = {}
-if __name__=='__main__':
-    processes = [Process(target = generateData, args = (distribution,)) for i in tqdm(range(16))]
-    for process in tqdm(processes):
-        process.start()
-    for process in tqdm(processes):
-        process.join()
+with mp.Manager() as manager:
+    import numpy as np
+    distribution = manager.dict()
+    with manager.Pool(16) as pool:
+        tqdm(pool.map(generateData, repeat(distribution, 50_000_000)))
 
 # In[232]:
 
 
-stat = {}
-for eed in distribution:
-    if len(distribution[eed]) > 10:
-        stat[eed] = np.mean(distribution[eed])
+    stat = {}
+    for eed in distribution:
+        if len(distribution[eed]) > 10:
+            stat[eed] = np.mean(distribution[eed])
 
 
 # In[258]:
 
 
-distribution4 = []
-with open("distribution4.csv", "w") as w:
-    for eed in stat:
-        x = eed[0]; y = eed[1]
-        A, ld = A_ld(30,20,50,x,y)
-        mm = pair_correlation(30,20,50,x,y)[0]
-        distribution4.append((stat[eed], mm))
-        w.write(str(x) + "," + str(y) + "," + str(stat[eed]) + "," + str(mm) + "\n")
+    distribution4 = []
+    with open("distribution4.csv", "w") as w:
+        for eed in stat:
+            x = eed[0]; y = eed[1]
+            A, ld = A_ld(30,20,50,x,y)
+            mm = pair_correlation(30,20,50,x,y)[0]
+            distribution4.append((stat[eed], mm))
+            w.write(str(x) + "," + str(y) + "," + str(stat[eed]) + "," + str(mm) + "\n")
 
 
 # In[259]:
 
 
-try:
-    x,y = zip(*distribution4)
-except Exception:
-    print("Not enough samples, subsitute with dummy samples")
-    x, y = [1,2], [1,2]
-plt.scatter(x=x, y=y,s=5)
-# plt.show()
-plt.savefig("correlation_3D_mm.png")
+    try:
+        x,y = zip(*distribution4)
+    except Exception:
+        print("Not enough samples, subsitute with dummy samples")
+        x, y = [1,2], [1,2]
+    plt.scatter(x=x, y=y,s=5)
+    # plt.show()
+    plt.savefig("correlation_3D_mm.png")
 
 
 # In[ ]:
 
 
-a,b = np.polyfit(x, y , 1)
-print(a,b)
+    a,b = np.polyfit(x, y , 1)
+    print(a,b)
